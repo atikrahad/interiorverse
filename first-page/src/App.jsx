a9c4fa76e1286f316eca361b6026342181dd2eff
Atik Rahad <atikrahad1@gmail.com>
2024-06-05 09:17:55 +0600

    initial commit with react project

---
+++ first-page/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <div className="bg-black">
+      <div className="max-w-[1440px] rounded-b-[4rem] min-h-screen mx-auto bg-[#F9EBFF]">
+        <div className="min-h-[80vh] bg-[#340253] rounded-b-[4rem]">full content</div>
+        <div>Footer</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
